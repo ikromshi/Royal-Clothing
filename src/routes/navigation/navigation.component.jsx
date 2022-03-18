6b3e715200c7782c6838899314107e7d904e9b77
Ikrom Numonov <numanovikrom454@gmail.com>
2022-03-18 19:10:06 -0400

    styling the nav bar and adding logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,22 @@
-import { Outlet} from "react-router-dom";
- 
+import "./navigation.styles.scss";
+
+import { Outlet, Link } from "react-router-dom"; // Wrapping elements with Fragment eqv of <> </>
+import { Fragment } from "react";
+import { ReactComponent as CrownLogo} from "../../assets/crown.svg"; {/** React allows SVG's to be imported as components */}
+
 const Navigation = () => {
     return (
-      <div>
-        <div>
-          <h1>I am the Navigation Bar</h1>
+      <Fragment>
+        <div className="navigation">
+          <Link className="logo-container" to="/">
+            <CrownLogo /> {/** Using the logo as a component */}
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-links" to="/shop">SHOP</Link>
+          </div>
         </div>
         <Outlet />
-      </div>
+      </Fragment>
     )
   }
 
