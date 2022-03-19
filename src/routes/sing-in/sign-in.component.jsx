a1d459419f8ce3b304330835a6b47c07928f34e7
Ikrom Numonov <numanovikrom454@gmail.com>
2022-03-19 00:02:00 -0400

    added Firebase authentication with google and set up the firestore data base

--- src/routes/sing-in/sign-in.component.jsx
+++ src/routes/sing-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
- 
+import { signInWithGooglePopup, createUserDocFromAuth } from "../../utils/firebase/firebase.utils";  
+
 const SignIn = () => {
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
-        console.log(response);
+        const userDocRef = await createUserDocFromAuth(response.user);
     } 
 
     return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore,  doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,6 +22,36 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+// Working with FIrestore Data Base
+export const db = getFirestore();
+export const createUserDocFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    // IF USER SNAPSHOT EXISTS, DO NOTHING; ELSE, CREATE THE DOCUMENT WITH THE DATA FROM USERAUTH IN THE COLLECTION
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName, 
+                email, 
+                createdAt
+            });
+        } catch {
+            console.log(`Error creating the user`)
+        }
+    }
+
+    return userDocRef;
+}
 
 /**
  * Firebase Config allows users to make CRUD actions on firebase
