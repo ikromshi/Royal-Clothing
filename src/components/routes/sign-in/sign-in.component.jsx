102ef02a29b6473df01e4caf2791eb90413a0722
Ikrom Numonov <numanovikrom454@gmail.com>
2022-05-22 19:32:59 -0400

    brought in firebase libraries and set up sign-in with google pupup

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import { signInWithGooglePopup } from "../../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup, createUserDocFromAuth } from "../../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocFromAuth(user);
     }
     return (
         <div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,13 @@
+// FIREBASE
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+
+// FIRESTORE
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+
+// **FIRESTORE
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDTKMS1iLA5y05sNyZOop0eaboiYtEDJvU",
@@ -22,4 +30,32 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
   
+// **FIREBASE
+export const db = getFirestore();
+export const createUserDocFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    console.log(userDocRef);
 
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot.exists());
+
+
+    // if user data does not exist, create/set the document with the data from userAuth in my collection
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch(error) {
+            console.log("error creating the user: ", error.message);
+        }
+    }
+    
+    // if user exists, just return userDocRef
+    return userDocRef;
+}
