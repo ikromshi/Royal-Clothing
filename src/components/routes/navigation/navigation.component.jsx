ed2fedda5fbbd5a850f550ba449bad3bde5e431e
Ikrom Numonov <numanovikrom454@gmail.com>
2022-05-22 10:56:37 -0400

    added styling to the navigation bar, improved index.scss

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
     return (
       <Fragment>
         <div className="navigation">
             <Link className="navigation" to="/">
-                <div>Logo</div>
+                <CrwnLogo className="logo"/>
             </Link>
             <div className="nav-links-container">
                 <Link className="nav-link" to="/shop">
