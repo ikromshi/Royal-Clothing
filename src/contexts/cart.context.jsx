93d19357ee7fe1d944e4fb3d7117cd66d9b95765
Ikrom Numonov <numanovikrom454@gmail.com>
2022-06-07 10:55:42 -0400

    extended cart context and made it so I can add items to cart-dropdown

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,51 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    // find if "cartItems" contains productToAdd;
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === productToAdd.id);
+
+    // if found, increment quantity and return the array;
+    if (existingCartItem) {
+        return cartItems.map(cartItem => cartItem.id === existingCartItem.id ? 
+            {...cartItem, quantity: cartItem.quantity + 1} : cartItem);
+    }
+
+    // else, return a new array with modified cartItems/ new cart item;
+    return [...cartItems, {...productToAdd, quantity: 1}];
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 })
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
+
+/**
+ product {
+    id,
+    name,
+    price,
+    imgUrl
+ }
+
+ CartItem {
+    id, 
+    name,
+    price, 
+    imgUrl, 
+    quantity
+ }
+ */
