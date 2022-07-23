d957c05e23c6b80a7976cc25a64b9964e66e523c
Ikrom Numonov <numanovikrom454@gmail.com>
2022-07-23 16:07:10 -0400

    set up stripe payment method on the checkout page

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -10,12 +10,38 @@ const PaymentForm = () => {
     e.preventDefault();
 
     if (!stripe || !elements) return;
-  }
 
+    const response =  await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ amount: 10000 })
+    }).then(res => res.json());
+
+    const { paymentIntent: { client_secret }} = response;
+    console.log(client_secret);
+
+    const paymentResult = stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Ikrom Numonov"
+        }
+      }
+    });
+
+    if (paymentResult.error) alert(paymentResult.error);
+    else {
+      if ((await paymentResult).paymentIntent.status === "succeeded") alert("Payment Successful");
+    }
+  };
+
+  
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
