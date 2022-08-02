089b08373f56d4c8f371d4f78bb09d2f413d2e63
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-02 19:31:54 -0400

    typed the whole user folder

--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -1,23 +1,30 @@
+import { AnyAction } from "redux";
+import { UserData } from "../../utils/firebase/firebase.utils";
 import { USER_ACTION_TYPES } from "./user.types";
 
-const INITIAL_STATE = {
+export type UserState = {
+  readonly currentUser: UserData | null;
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+const INITIAL_STATE: UserState = {
   currentUser: null, 
   isLoading: false,
   error: null
 };
 
-export const userReducer = (state=INITIAL_STATE, action) => {
-  const { type, payload } = action;
+export const userReducer = (state=INITIAL_STATE, action: AnyAction) => {
 
-  switch(type) {
+  switch(action.type) {
     case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return {...state, currentUser: payload};
+      return {...state, currentUser: action.payload};
     case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
       return {...state, currentUser: null};
+    case USER_ACTION_TYPES.SIGN_IN_FAILED:
+      return {...state, error: action.payload};
     case USER_ACTION_TYPES.SIGN_OUT_FAILED:
     case USER_ACTION_TYPES.SIGN_UP_FAILED:
-    case USER_ACTION_TYPES.SIGN_IN_FAILED:
-      return {...state, error: payload};
     default:
       return state;
   };
