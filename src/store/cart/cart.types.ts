6b6c72d2714d1850176f71d58e900a4931140b02
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-02 16:56:19 -0400

    typed cart action and cart types in ts

---
+++ src/store/cart/cart.types.ts
@@ -0,0 +1,13 @@
+import { CategoryItem } from "../categories/category.types";
+
+export enum CART_ACTION_TYPES {
+  SET_CART_ITEMS = "cart/SET_CART_ITEMS",
+  SET_IS_CART_OPEN = "cart/SET_IS_CART_OPEN",
+  SET_CART_COUNT = "cart/SET_CART_COUNT",
+  SET_CART_TOTAL = "cart/SET_CART_TOTAL",
+  EMPTY_ITEMS = "cart/EMPTY_ITEMS"
+};
+
+export type CartItem = CategoryItem & {
+  quantity: number;
+};
