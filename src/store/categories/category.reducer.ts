44b03bf9e60ebff22155c76d602de80a92dcf86d
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-02 16:16:07 -0400

    typed reducer.utils, category action/reducer/selector

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,5 +1,6 @@
-import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
-import { CategoryAction } from "./category.action";
+import { AnyAction } from "redux";
+import { fetchCategoriesStart, fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
+import { Category } from "./category.types";
 
 export type CategoryState = {
   readonly categories: Category[];
@@ -13,16 +14,30 @@ const INITIAL_STATE: CategoryState = {
   error: null // potential error in the fetching process of the async function
 };
 
-export const categoriesReducer = (state=INITIAL_STATE, action={} as CategoryAction) => {
+export const categoriesReducer = (state=INITIAL_STATE, action={} as AnyAction) => {
 
-  switch(action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {...state, isLoading: true};
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return {...state, categories: action.payload, isLoading: false};
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return {...state, error: action.payload, isLoading: false};
-    default:
-      return state;
+  if (fetchCategoriesStart.match(action)) {
+    return {...state, isLoading: true};
   };
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return {...state, categories: action.payload, isLoading: false};
+  };
+
+  if (fetchCategoriesFailed.match(action)) {
+    return {...state, error: action.payload, isLoading: false};
+  };
+
+  return state;
+
+  // switch(action.type) {
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+  //     return {...state, isLoading: true};
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+  //     return {...state, categories: action.payload, isLoading: false};
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+  //     return {...state, error: action.payload, isLoading: false};
+  //   default:
+  //     return state;
+  // };
 };
