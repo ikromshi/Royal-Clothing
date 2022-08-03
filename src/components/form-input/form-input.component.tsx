359736cb0d8d12c4939ac4dc39ebb871bc785542
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-03 12:12:36 -0400

    typed the form input  component

---
+++ src/components/form-input/form-input.component.tsx
@@ -0,0 +1,19 @@
+import { FC, InputHTMLAttributes } from "react";
+import { FormInputLabel, Input, Group } from  "./form-input.styles.jsx";
+
+type FormInputProps = {label: string} & InputHTMLAttributes<HTMLInputElement>;
+
+const FormInput: FC<FormInputProps> = ({ label, ...otherProps }) => {
+  return (
+  <Group>
+    <Input {...otherProps} />
+      {label && (
+    <FormInputLabel 
+      shrink={Boolean(otherProps.value && typeof otherProps.value === "string" && otherProps.value.length)}>
+      {label}
+    </FormInputLabel>)}
+    </Group>
+  )
+}
+
+export default FormInput; 
