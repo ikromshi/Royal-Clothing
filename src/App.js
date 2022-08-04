0f059ff2f70745ececa1d9fe7f5dc36d742c484b
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-04 16:31:45 -0400

    global.styles.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { checkUserSession } from "./store/user/user.action";
-import { Routes, Route } from "react-router-dom"; 
-import { useDispatch } from "react-redux";
-import {  lazy, Suspense, useEffect } from "react";
 import Spinner from "./components/spinner/spinner.component";
-import CheckOut from "./routes/check-out/check-out.component";
+import { checkUserSession } from "./store/user/user.action";
+import {  lazy, Suspense, useEffect } from "react";
+import { Routes, Route } from "react-router-dom"; 
+import { GlobalStyle } from "./global.styles";
+import { useDispatch } from "react-redux";
 
 const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
 const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
-// const CheckOut = lazy(() => import("./routes/check-out/check-out.component"));
+const CheckOut = lazy(() => import("./routes/check-out/check-out.component"));
 const Home = lazy(() => import("./routes/home/home.component"));
 const Shop = lazy(() => import("./routes/shop/shop.component"));
+
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,14 +21,15 @@ const App = () => {
 
   return ( 
     <Suspense fallback={<Spinner />}>
-      <Routes>
-        <Route path="/" element={<Navigation />} >
-          <Route index element={<Home />} /> {/**Set the index to true to show Home on the default page without navigating to it */}
-          <Route path="shop/*" element={<Shop />} />
-          <Route path="auth" element={<Authentication />} />
-          <Route path="checkout" element={<CheckOut />} />
-        </Route>
-      </Routes>
+      <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Navigation />} >
+            <Route index element={<Home />} /> {/**Set the index to true to show Home on the default page without navigating to it */}
+            <Route path="shop/*" element={<Shop />} />
+            <Route path="auth" element={<Authentication />} />
+            <Route path="checkout" element={<CheckOut />} />
+          </Route>
+        </Routes>
     </Suspense>
   );
 };
