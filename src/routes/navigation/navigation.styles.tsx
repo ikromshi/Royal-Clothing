38e410a0a1f14e4564ab419d61a8fadba15d2861
Ikrom Numonov <numanovikrom454@gmail.com>
2022-08-06 12:05:00 -0400

    made the directory and navigation components mobile compatible

--- src/routes/navigation/navigation.styles.tsx
+++ src/routes/navigation/navigation.styles.tsx
@@ -8,12 +8,23 @@ export const NavigationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    heigh: 60px;
+    margin-bottom: 20px;
+    padding: 10px   
+  }
 `
 // Styling over an existing component
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0;
+  }
 `
 export const NavLinks = styled.div`
   width: 50%;
@@ -21,6 +32,10 @@ export const NavLinks = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `
 export const NavLink = styled(Link)`  
   padding: 10px 15px;
